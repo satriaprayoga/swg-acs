1d876606776c2c7ee2689110ad38930e94cb581f
satriaprayoga <satria.prayoga@gmail.com>
2012-06-25 02:38:08 +0700

    commenting

--- swg-acs-core/message/src/main/java/com/swg/acs/Argument.java
+++ swg-acs-core/message/src/main/java/com/swg/acs/Argument.java
@@ -5,20 +5,55 @@ package com.swg.acs;
 
 
 /**
+ * Interface buat mengolah element SOAP-XML
+ * 
  * @author satriaprayoga
  *
  */
 public interface Argument {
 
-	
+	/**
+	 * setting attribute element
+	 * @param name
+	 * @param value
+	 */
 	void setAttribute(String name,String value);
-	boolean hasChild();
+	/**
+	 * setting nilai dari element ini
+	 * @param value
+	 * @return
+	 */
 	Argument setValue(String value);
+	/**
+	 * ambil nilai dari element ini
+	 * @return
+	 */
 	String getValue();
+	/**
+	 * ambil nilai dari attribute ini, dengan input nama attributnya
+	 * @param name
+	 * @return
+	 */
 	String getAttribute(String name);
+	/**
+	 * nilai nama atribut
+	 * @return
+	 */
 	String getAttributeName();
-	int childSize();
+	
+	/**
+	 * nama dari element ini
+	 * @return
+	 */
 	String getName();
+	/**
+	 * URI dari element ini (coba baca lagi tentang SOAP)
+	 * @return
+	 */
 	String getUri();
+	/**
+	 * prefix dari element ini, biasa xsi,xsd, atau userdefined prefix
+	 * @return
+	 */
 	String getPrefix();
 }
