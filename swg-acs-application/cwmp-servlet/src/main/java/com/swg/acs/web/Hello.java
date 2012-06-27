99db4a8b5983cf45084a6f21e0423325f496a252
satriaprayoga <satria.prayoga@gmail.com>
2012-06-27 14:06:21 +0700

    cwmp servlet created

---
+++ swg-acs-application/cwmp-servlet/src/main/java/com/swg/acs/web/Hello.java
@@ -0,0 +1,34 @@
+package com.swg.acs.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Hello extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	static String PAGE_HEADER = "<html><head><title>helloworld</title><body>";
+
+	static String PAGE_FOOTER = "</body></html>";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter writer=resp.getWriter();
+		writer.println(PAGE_HEADER);
+		writer.println("<h1>"+"Test" +"</h1>");
+		writer.println(PAGE_FOOTER);
+		writer.close();
+	}
+
+}
